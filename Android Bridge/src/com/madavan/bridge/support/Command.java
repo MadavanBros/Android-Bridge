b35fd89fbaaabd1ee6e4432d55018bcfb2a88add
ashwin153 <ashwin.madavan@gmail.com>
2013-04-14 15:46:24 -0700

    Update Command.java

--- Android Bridge/src/com/madavan/bridge/support/Command.java
+++ Android Bridge/src/com/madavan/bridge/support/Command.java
@@ -1,5 +1,11 @@
 package com.madavan.bridge.support;
 
 public enum Command {
-  BID, CARD;
+  BID,          // Tells the user/server the most recent (and highest) bid (BID)
+  DEAL,         // Tells the user that a card has been dealt to them (CARD)
+  PLAY_TURN,    // Tells a user that it is his/her turn to play ("")
+  BID_TURN,     // Tells a user that it is his/her turn to bid ("")
+  CARD;         // Tells a user/server a card has been played (PLAYER, CARD)
+  
+  
 }
