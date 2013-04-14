ad64f2bf5f5128f42b22df34c79b4c67cdb9215e
Avinash <avinash.madavan@gmail.com>
2013-04-14 16:10:11 -0700

    Added send method to bridge thread

--- Bridge-Server/src/com/madavan/bridge/server/BridgeThread.java
+++ Bridge-Server/src/com/madavan/bridge/server/BridgeThread.java
@@ -4,8 +4,10 @@ package com.madavan.bridge.server;
 import java.util.ArrayList;
 import java.io.IOException;
 
+import com.madavan.bridge.cards.Bid;
 import com.madavan.bridge.cards.Card;
 import com.madavan.bridge.cards.Deck;
+import com.madavan.bridge.cards.Rank;
 import com.madavan.bridge.cards.Suit;
 import com.madavan.bridge.support.Command;
 
@@ -25,50 +27,54 @@ public class BridgeThread extends Thread {
   
   @Override
   public void run() {
+	  try {
     // Tell players game has begun
 	  dealDeck(); // Reset, shuffle, and deal deck
     
     // Bidding
     //  - Pass is the bid CLUBS,0 (or we could do PASS)
     //  - Use Rank, Suit compareTo methods
+	  bid();
     
     // Play Game
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
   }
   
-  private void dealDeck() {
+  private void dealDeck() throws IOException {
 	  _deck.reset();
 	  _deck.shuffle();
 
 	  _curPlayer = _curDealer + 1;
 	  for ( int i = 0; i < 52; i++ )
-		  _players.get((_curPlayer++)%4).send(Command.CARD+":"+_deck.getNext()+".");
+		  send((_curPlayer++)%4, Command.CARD, _deck.getNext().toString());
   }
   
-  private void bid() {
+  private void bid() throws IOException {
 	  _curPlayer = _curDealer;
 	  int numPasses = 0;
+	  Bid greatestBid = null;
+	  Bid bid;
 	  while ( numPasses < 3 ) {
-		  _players.get(_curPlayer).send(Command.BID+":GIVE ME YO BID.");
-		  String sbid = _players.get((_curPlayer++)%4).readNext(); // TRY CATCH BLOCK NEEDED
+		  send(_curPlayer, Command.BID_TURN, "");
+		  String sbid = _players.get((_curPlayer++)%4).readNext();
 		  if (sbid.equals("PASS"))
 			  numPasses++;
 		  else {
-			  String[] bidParts = sbid.split(",");
-			  Suit s = Suit.valueOf(bidParts[0]);
-			  int num = Integer.valueOf(bidParts[1]);
-			  
-			  // Backup check to see if the bid is valid
-			  if ( num <  )
+			  bid = Bid.fromString(sbid);
+			  if ( greatestBid == null || bid.compareTo(greatestBid) > 0 )
+				  greatestBid = bid;
 		  }
 	  }
   }
   
   private int compareCard(Card c1, Card c2, Suit trump, Suit trick) {
-    int rankCompare = _rank.compareTo(oth.getRank());
-    boolean c1Trump = card1.getSuit().equals(trump);
-    boolean c2Trump = card2.getSuit().equals(trump);
-	boolean c1Trick = card1.getSuit().equals(trick);
-    boolean c2Trick = card2.getSuit().equals(trick);
+    int rankCompare = c1.getRank().compareTo(c2.getRank());
+    boolean c1Trump = c1.getSuit().equals(trump);
+    boolean c2Trump = c2.getSuit().equals(trump);
+	boolean c1Trick = c1.getSuit().equals(trick);
+    boolean c2Trick = c2.getSuit().equals(trick);
     
     if(c1Trump && c2Trump)
       return rankCompare;
@@ -83,5 +89,9 @@ public class BridgeThread extends Thread {
       return -1;
     else
       return rankCompare;
+  }
+  
+  private void send(int player, Command c, String msg) throws IOException {
+	  _players.get(player).send(c+":"+msg+".");
   }
 }
