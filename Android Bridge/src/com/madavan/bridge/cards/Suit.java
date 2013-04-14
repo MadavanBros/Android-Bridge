f190091f1caa3f166ce73c99ed63d059712e4473
ashwin153 <ashwin.madavan@gmail.com>
2013-04-14 10:14:13 -0700

    Update Suit.java

--- Android Bridge/src/com/madavan/bridge/cards/Suit.java
+++ Android Bridge/src/com/madavan/bridge/cards/Suit.java
@@ -2,44 +2,6 @@ package com.madavan.bridge.cards;
 
 public enum Suit {
 
-	CLUBS, DIAMONDS, HEARTS, SPADES, NO_TRUMP;
+	CLUBS, DIAMONDS, HEARTS, SPADES;
 	
-	// Is this suit the trump suit.
-	private boolean _isTrump;
-	// Is this suit the suit of the trick.
-	private boolean _isTrick;
-	
-	public boolean isTrump() {
-		return _isTrump;
-	}
-	
-	// Sets this Suit as the Trump card, making sure
-	// to set the current Trump to false.
-	public void setTrump() {
-		for(Suit s : Suit.values())
-			s.setTrump(false);
-		
-		_isTrump = true;
-	}
-	
-	public boolean isTrick() {
-		return _isTrick;
-	}
-
-	// Sets this Suit as the Trick card, making sure
-	// to set the current Trick to false.
-	public void setTrick() {
-		for(Suit s : Suit.values())
-			s.setTrick(false);
-				
-		_isTrick = true;
-	}
-	
-	private void setTrick(boolean isTrick) {
-		_isTrick = isTrick;
-	}
-	
-	private void setTrump(boolean isTrump) {
-		_isTrump = isTrump;
-	}
 }
