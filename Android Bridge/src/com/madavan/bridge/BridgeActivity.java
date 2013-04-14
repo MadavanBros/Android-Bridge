fa393d665aa5d50e018604f3684c4c4ca7d1eb92
ashwin153 <ashwin.madavan@gmail.com>
2013-04-13 23:27:18 -0700

    Added BridgeActivity and associated layout file (activity_bridge.xml) and updated AndroidManifest and strings to reflect these changes.

---
+++ Android Bridge/src/com/madavan/bridge/BridgeActivity.java
@@ -0,0 +1,13 @@
+package com.madavan.bridge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BridgeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
