4879d17ac72a0d3a3a362dd12d508bd35126630b
ashwin153 <ashwin.madavan@gmail.com>
2013-04-14 12:17:06 -0700

    Update Rank.java

--- Android Bridge/src/com/madavan/bridge/cards/Rank.java
+++ Android Bridge/src/com/madavan/bridge/cards/Rank.java
@@ -2,7 +2,7 @@ package com.madavan.bridge.cards;
 
 public enum Rank {
 
-	DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, 
+	TWO, THREE, FOUR, FIVE, SIX, SEVEN, 
 	EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
 
 }
