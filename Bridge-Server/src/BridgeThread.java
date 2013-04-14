ec683fa5154a186f7cbce33a81e146beb2913519
ashwin153 <ashwin.madavan@gmail.com>
2013-04-14 09:11:36 -0700

    Update BridgeThread.java

--- Bridge-Server/src/BridgeThread.java
+++ Bridge-Server/src/BridgeThread.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.io.IOException;
 
-public class BridgeThread {
+public class BridgeThread extends Thread {
 
+  private ArrayList<BridgePlayer> _players;
+  
+  public BridgeThread(ArrayList<BridgePlayer> players) {
+    _players = players;
+  }
+  
+  @Override
+  public void run() {
+    
+  }
 }
