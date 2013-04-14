01003b2d5b94b34f6d73601373bb75fccec937d6
ashwin153 <ashwin.madavan@gmail.com>
2013-04-14 12:15:22 -0700

    Update Deck.java

--- Bridge-Server/src/com/madavan/bridge/cards/Deck.java
+++ Bridge-Server/src/com/madavan/bridge/cards/Deck.java
@@ -1,1 +1,34 @@
-Dec
+package com.madavan.bridge.cards;
+import java.util.Collections;
+import java.util.Stack;
+
+
+public class Deck {
+
+  private Stack<Card> _cards;
+
+	public Deck() {
+		reset();
+	}
+
+	public void shuffle() {
+		Collections.shuffle(_cards);
+	}
+
+	public Card getNext() {
+		return _cards.pop();
+	}
+
+	public int getNumCards() {
+		return _cards.size();
+	}
+
+	public void reset() {
+		_cards = new Stack<Card>();
+
+		for(Rank rank : Rank.values())
+			for(Suit suit : Suit.values())
+				if(suit != Suit.NO_TRUMP)
+					_cards.push(new Card(rank, suit));
+	}
+}
