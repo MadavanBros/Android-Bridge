38c11d5ca16e3dc255c6072731e74c5bb05d2dde
Avinash <avinash.madavan@gmail.com>
2013-04-14 16:16:14 -0700

    Changed Integer to int in bid classes

--- Bridge-Server/src/com/madavan/bridge/cards/Bid.java
+++ Bridge-Server/src/com/madavan/bridge/cards/Bid.java
@@ -2,48 +2,51 @@ package com.madavan.bridge.cards;
 
 public class Bid implements Comparable<Bid> {
 
-  private Integer _num;
-  private Suit _suit;
-  private Integer _player;
+	private int _num;
+	private Suit _suit;
+	private int _player;
 
-  public Bid(int player, int num, Suit suit) {
-	  _player = player;
-    _num = num;
-    _suit = suit;
-  }
+	public Bid(int player, int num, Suit suit) {
+		_player = player;
+		_num = num;
+		_suit = suit;
+	}
 
-  public int getRank() {
-    return _num;
-  }
+	public int getNum() {
+		return _num;
+	}
 
-  public Suit getSuit() {
-    return _suit;
-  }
-  
-  public int getPlayer() {
-	  return _player;
-  }
+	public Suit getSuit() {
+		return _suit;
+	}
 
-  @Override
-  public int compareTo(Bid oth) {
-    if(_num.compareTo(oth.getRank()) > 0 ||
-       _num.compareTo(oth.getRank()) == 0 &&
-       _suit.compareTo(oth.getSuit()) >  0)
-      return 1;
-    else if(_num.compareTo(oth.getRank()) == 0 &&
-            _suit.compareTo(oth.getSuit()) == 0)
-      return 0;
-    else
-      return -1;
-  }
+	public int getPlayer() {
+		return _player;
+	}
 
-  @Override
-  public String toString() {
-    return _player.toString() + "," + _num.toString() + "," + _suit.toString();
-  }
-  
-  public static Bid fromString(String str) {
-    String[] val = str.split(",");
-    return new Bid(Integer.valueOf(val[0]), Integer.valueOf(val[1]), Suit.valueOf(val[2]));
-  }
+	@Override
+	public int compareTo(Bid oth) {
+		if (_num > oth.getNum()
+				|| _num == oth.getNum()
+				&& _suit.compareTo(oth.getSuit()) > 0)
+			return 1;
+		else if (_num == oth.getNum()
+				&& _suit.compareTo(oth.getSuit()) == 0)
+			return 0;
+		else
+			return -1;
+	}
+
+	@Override
+	public String toString() {
+		return _player + "," + _num + ","
+				+ _suit.toString();
+	}
+
+	public static Bid fromString(String str) {
+		String[] val = str.split(",");
+		return new Bid(Integer.valueOf(val[0]), Integer.valueOf(val[1]),
+				Suit.valueOf(val[2]));
+	}
+
 }
